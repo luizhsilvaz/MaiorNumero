f1c580e051c7d72c687ba73f7f1725074232bc02
lh7067270 <33893056-lh7067270@users.noreply.replit.com>
2024-05-21 22:35:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+n1 = float(input("Digite um numero"))
+n2 = float(input("Digite outro numero"))
+
+if n1 > n2:
+  print(f"O maior numero e o {n1}")
+else:
+  print(f"O maior numero e o {n2}")
+           
